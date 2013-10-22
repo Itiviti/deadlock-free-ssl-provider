d1f663c046e327a35e7083f5a5abb19b2a5d49d2
Zipeng Wu <zipeng.wu@ullink.com>
2013-10-22 11:33:34 +0200

    native plug for whindows

--- src/main/java/ullink/security/provider/NativeSeedGenerator.java
+++ src/main/java/ullink/security/provider/NativeSeedGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,14 +28,39 @@ package ullink.security.provider;
 import java.io.IOException;
 
 /**
- * Native seed generator for Unix systems. Inherit everything from
- * URLSeedGenerator.
+ * Seed generator for Windows making use of MS CryptoAPI using native code.
  *
  */
-class NativeSeedGenerator extends SeedGenerator.URLSeedGenerator {
+class NativeSeedGenerator extends SeedGenerator {
 
+    /**
+     * Create a new CryptoAPI seed generator instances.
+     *
+     * @exception IOException if CryptoAPI seeds are not available
+     * on this platform.
+     */
     NativeSeedGenerator() throws IOException {
         super();
+        // try generating two random bytes to see if CAPI is available
+        if (!nativeGenerateSeed(new byte[2])) {
+            throw new IOException("Required native CryptoAPI features not "
+                                  + " available on this machine");
+        }
+    }
+
+    /**
+     * Native method to do the actual work.
+     */
+    private static native boolean nativeGenerateSeed(byte[] result);
+
+    @Override
+    void getSeedBytes(byte[] result) {
+        // fill array as a side effect
+        if (nativeGenerateSeed(result) == false) {
+            // should never happen if constructor check succeeds
+            throw new InternalError
+                            ("Unexpected CryptoAPI failure generating seed");
+        }
     }
 
 }
